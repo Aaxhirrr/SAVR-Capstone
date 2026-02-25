a70eb31559f7fa98ec3381d1dff7416eb1733486
Aashir Javed <Aaxhirrr@users.noreply.github.com>
2026-02-24 23:18:32 -0700

    Add SavrMobile/DesignSystem/SavrTypography.swift

---
+++ SavrMobile/DesignSystem/SavrTypography.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+enum SavrTypography {
+    static let heroTitle = Font.system(size: 34, weight: .black, design: .rounded)
+    static let sectionTitle = Font.system(size: 28, weight: .black, design: .rounded)
+    static let cardTitle = Font.system(size: 18, weight: .bold, design: .rounded)
+
+    static let body = Font.system(size: 16, weight: .regular, design: .rounded)
+    static let caption = Font.system(size: 13, weight: .medium, design: .rounded)
+}
